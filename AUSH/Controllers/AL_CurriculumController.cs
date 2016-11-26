0f9c52a5bc47454c156b8249323e94515072a459
YohanKing <ykavindu@yahoo.com>
2016-11-26 19:02:51 +0530

    OL Curriculum and AL Curriculum Added

---
+++ AUSH/Controllers/AL_CurriculumController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using AUSH;
+
+namespace AUSH.Views
+{
+    public class AL_CurriculumController : Controller
+    {
+        private AUSH_DBEntities db = new AUSH_DBEntities();
+
+        // GET: AL_Curriculum
+        public ActionResult Index()
+        {
+            return View(db.AL_Curriculum.ToList());
+        }
+
+        // GET: AL_Curriculum/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            AL_Curriculum aL_Curriculum = db.AL_Curriculum.Find(id);
+            if (aL_Curriculum == null)
+            {
+                return HttpNotFound();
+            }
+            return View(aL_Curriculum);
+        }
+
+        // GET: AL_Curriculum/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: AL_Curriculum/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ALCurID,Name,Description,ZorMean")] AL_Curriculum aL_Curriculum)
+        {
+            if (ModelState.IsValid)
+            {
+                db.AL_Curriculum.Add(aL_Curriculum);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(aL_Curriculum);
+        }
+
+        // GET: AL_Curriculum/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            AL_Curriculum aL_Curriculum = db.AL_Curriculum.Find(id);
+            if (aL_Curriculum == null)
+            {
+                return HttpNotFound();
+            }
+            return View(aL_Curriculum);
+        }
+
+        // POST: AL_Curriculum/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ALCurID,Name,Description,ZorMean")] AL_Curriculum aL_Curriculum)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(aL_Curriculum).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(aL_Curriculum);
+        }
+
+        // GET: AL_Curriculum/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            AL_Curriculum aL_Curriculum = db.AL_Curriculum.Find(id);
+            if (aL_Curriculum == null)
+            {
+                return HttpNotFound();
+            }
+            return View(aL_Curriculum);
+        }
+
+        // POST: AL_Curriculum/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            AL_Curriculum aL_Curriculum = db.AL_Curriculum.Find(id);
+            db.AL_Curriculum.Remove(aL_Curriculum);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ AUSH/Controllers/OL_CurriculumController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using AUSH;
+
+namespace AUSH.Views
+{
+    public class OL_CurriculumController : Controller
+    {
+        private AUSH_DBEntities db = new AUSH_DBEntities();
+
+        // GET: OL_Curriculum
+        public ActionResult Index()
+        {
+            return View(db.OL_Curriculum.ToList());
+        }
+
+        // GET: OL_Curriculum/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            OL_Curriculum oL_Curriculum = db.OL_Curriculum.Find(id);
+            if (oL_Curriculum == null)
+            {
+                return HttpNotFound();
+            }
+            return View(oL_Curriculum);
+        }
+
+        // GET: OL_Curriculum/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: OL_Curriculum/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "OLCurID,Name,Description")] OL_Curriculum oL_Curriculum)
+        {
+            if (ModelState.IsValid)
+            {
+                db.OL_Curriculum.Add(oL_Curriculum);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(oL_Curriculum);
+        }
+
+        // GET: OL_Curriculum/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            OL_Curriculum oL_Curriculum = db.OL_Curriculum.Find(id);
+            if (oL_Curriculum == null)
+            {
+                return HttpNotFound();
+            }
+            return View(oL_Curriculum);
+        }
+
+        // POST: OL_Curriculum/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "OLCurID,Name,Description")] OL_Curriculum oL_Curriculum)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(oL_Curriculum).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(oL_Curriculum);
+        }
+
+        // GET: OL_Curriculum/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            OL_Curriculum oL_Curriculum = db.OL_Curriculum.Find(id);
+            if (oL_Curriculum == null)
+            {
+                return HttpNotFound();
+            }
+            return View(oL_Curriculum);
+        }
+
+        // POST: OL_Curriculum/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            OL_Curriculum oL_Curriculum = db.OL_Curriculum.Find(id);
+            db.OL_Curriculum.Remove(oL_Curriculum);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
